1b20be5ce892cc58282e06033c18814fe43a52f4
Nikita Reshetniak <nikreshetnjak@gmail.com>
2018-09-08 17:38:06 +0200

    Add grid

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var mapSize = 10;
 var i, j;
 
-for(i = 0; i < mapSize; i++){
+for(i = 1; i <= mapSize; i++){
     var divRow = document.createElement("div");
-    divRow.className = "row row-" + i;
-    document.body.appendChild(divRow);
-    for (j = 0; j < mapSize; j++){
+    divRow.classList.add("row", "row-" + i);
+    mapWraper.appendChild(divRow);
+    for (j = 1; j <= mapSize; j++){
         var divCell = document.createElement("div");
-        divCell.className = "cell cell-" + j;
-        document.body.appendChild(divCell);
+        divCell.classList.add("cell", "cell-" + j);
+        divRow.appendChild(divCell);
     }
 }
