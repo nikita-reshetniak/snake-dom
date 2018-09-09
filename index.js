4cc13f475c9f8193224922038568c8c3f4a598ff
Nikita Reshetniak <nikreshetnjak@gmail.com>
2018-09-09 16:50:47 +0200

    Add snake to the center of grid

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var mapSize = 10;
+var gridSize = 11;
+var gridCenter = Math.round(gridSize/2);
 var i, j;
 
-for(i = 1; i <= mapSize; i++){
+for(i = 1; i <= gridSize; i++){
     var divRow = document.createElement("div");
     divRow.classList.add("row", "row-" + i);
-    mapWraper.appendChild(divRow);
-    for (j = 1; j <= mapSize; j++){
+    gridWraper.appendChild(divRow);
+    for (j = 1; j <= gridSize; j++){
         var divCell = document.createElement("div");
         divCell.classList.add("cell", "cell-" + j);
         divRow.appendChild(divCell);
     }
 }
+
+var activeRow = document.getElementsByClassName("row-" + gridCenter);
+var activeCell = activeRow[0].getElementsByClassName("cell-" + gridCenter);
+activeCell[0].classList.add("activeCell");
