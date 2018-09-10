a77b85061e65aed0ce2572b00da6356d85f13d95
Nikita Reshetniak <nikreshetnjak@gmail.com>
2018-09-10 15:39:59 +0200

    Added comments. Created for loop to move snake to the right wall.

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ for(i = 1; i <= gridSize; i++){
     }
 }
 
-var activeRow = document.getElementsByClassName("row-" + gridCenter);
-var activeCell = activeRow[0].getElementsByClassName("cell-" + gridCenter);
-activeCell[0].classList.add("activeCell");
+var centerRow = document.getElementsByClassName("row-" + gridCenter);       // Find central row 
+var centerCell = centerRow[0].getElementsByClassName("cell-" + gridCenter); // find central cell of that row
+centerCell[0].classList.add("activeCell");                                  // add class activeCell to the central cell for black background
+
+var centerRowCells = centerRow[0].children; // HTMLCollection with cells of center row
+
+for(i = 0; i < gridSize - 1; i++){
+    if(centerRowCells[i].classList.contains("activeCell")){ // Search for <div> with class activeCell
+        centerRowCells[i].classList.remove("activeCell");   // remove class activeCell from that <div>
+        centerRowCells[i + 1].classList.add("activeCell");  // add class activeCell to the next <div>
+    }
+}
