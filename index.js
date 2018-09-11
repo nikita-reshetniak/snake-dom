c388e304c05066fe09f042c11e2acb3504593c81
Nikita Reshetniak <nikreshetnjak@gmail.com>
2018-09-11 20:28:36 +0200

    Create Array wich contain cells. Added snake to the center of grid.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var gridSize = 11;
 var gridCenter = Math.round(gridSize/2);
-var i, j;
 
-for(i = 1; i <= gridSize; i++){
+var fragment = document.createDocumentFragment();
+for(var i = 1; i <= gridSize; i++){
     var divRow = document.createElement("div");
     divRow.classList.add("row", "row-" + i);
-    gridWraper.appendChild(divRow);
-    for (j = 1; j <= gridSize; j++){
+    for(var j = 1; j <= gridSize; j++){
         var divCell = document.createElement("div");
         divCell.classList.add("cell", "cell-" + j);
         divRow.appendChild(divCell);
     }
+    fragment.appendChild(divRow);
+}
+gridWrapper.appendChild(fragment);
+
+var rows = Array.from(document.getElementsByClassName("row"));
+var cells = [];
+
+for(var i = 0; i < gridSize; i++){
+    cells[i] = rows[i].children;
 }
 
-var activeRow = document.getElementsByClassName("row-" + gridCenter);
-var activeCell = activeRow[0].getElementsByClassName("cell-" + gridCenter);
-activeCell[0].classList.add("activeCell");
+cells[gridCenter - 1][gridCenter - 2].classList.add("activeCell");
+cells[gridCenter - 1][gridCenter - 1].classList.add("activeCell");
+cells[gridCenter - 1][gridCenter].classList.add("activeCell");
+
