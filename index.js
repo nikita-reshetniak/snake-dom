19e29ebad782cbe8108857085d658c4470ba3bad
Nikita Reshetniak <nikreshetnjak@gmail.com>
2018-10-03 20:18:59 +0200

    Resolved #6 issue.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ var yStep = 0;
 // For keydownHandler function
 var firstTime = true;
 
+var timeoutID;
+var idRequest;
+
 makeGrid();
 addCellsToArray();
 addSnake();
@@ -79,12 +82,17 @@ function removeSnakeCell(){
 }
 
 // Move the snake
-function snakeMove(){
-    setTimeout(function () {
-        requestAnimationFrame(snakeMove);
+function snakeMove() {
+    timeoutID = setTimeout(function () {
+        idRequest = requestAnimationFrame(snakeMove);
+        if (checkGameOver()) {
+            cancelAnimationFrame(idRequest);
+            clearTimeout(timeoutID);
+        } else {
         removeSnakeCell();
         addSnakeCell();
         changeSnakeCellCoordinates();
+        };
     }, 1000 / fps);
 }
 
@@ -116,16 +124,22 @@ function keydownHandler(e){
             }
             break;
     }
-    if(firstTime){
+    if(firstTime && (e.keyCode === 37 ||
+                    e.keyCode === 38 ||
+                    e.keyCode === 39 ||
+                    e.keyCode === 40)){
         snakeMove();
         firstTime = false;
     }
 }
 
 // Checks whether the snake hit the wall
-function checkGameOver(){
-    if(y + yStep > gridSize - 1 || y + yStep < 0 || x + xStep > gridSize - 1 || x + xStep < 0) {
-        document.querySelector("#message").innerHTML = "Game Over";
-        return;
+function checkGameOver() {
+    if (y + yStep > gridSize - 1 ||
+        y + yStep < 0 ||
+        x + xStep > gridSize - 1 ||
+        x + xStep < 0) {
+            document.querySelector("#message").innerHTML = "Game Over";
+            return true;
     }
 }
